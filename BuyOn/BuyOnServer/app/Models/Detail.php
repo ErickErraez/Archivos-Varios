271d6ad7e055a28783f770b66ca6c4ac346796d7
ErickErraez <edg.erraez@yavirac.edu.ec>
2019-02-02 21:11:16 -0500

    BuyOn

---
+++ BuyOn/BuyOnServer/app/Models/Detail.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Detail extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+       'price','quantity',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+       
+    ];
+
+}
